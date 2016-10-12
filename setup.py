bf779a1b0b072d60f8053a45b3ed1fbcb1bb594d
Stefan Midjich <swehack@gmail.com>
2016-10-12 15:27:28 +0200

    requirements added.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
+    install_requires=[
+        'requests'
+    ],
     name='veeamclient',
     version='1.0',
     py_modules=['veeamclient']
